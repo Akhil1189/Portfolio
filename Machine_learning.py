d30f5d6e98e3009dd8679025bfe0a1927d19d7f3
Akhilesh Yogendra Palve <71075316+Akhil1189@users.noreply.github.com>
2021-04-04 11:34:51 +0530

    Add files via upload
    Created a machine learning script using pythons matplotlib.pyplot library and machine learning modules

---
+++ Machine_learning.py
@@ -0,0 +1,33 @@
+import panda as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('california_housing_test')
+df.head()
+
+y = df['total_rooms']
+x = df [['longitude', 'latitude', 'housing_median_age']]
+
+def CasesReg(x,y):
+    from sklearn.linear_model import LinearRegression
+    reg = LinearRegression()
+    reg.fit(x,y)
+    return reg
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y)
+
+reg = CasesReg(x_train, y_train)
+
+
+y_pred = reg.predict(x_test)
+y_pred
+
+reg.score(x_test, y_test)
+
+
+plt.plot(x, reg.predict(x), '*')
+plt.legend(labels=['longitude', 'latitude', 'housing_median_age'])
+plt.xlabel("Long Rate")
+plt.ylabel("total_rooms")
+plt.show()
